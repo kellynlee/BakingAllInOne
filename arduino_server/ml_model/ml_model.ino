5c09f0dec5ddd6fd61f574e7f1ed45f1d56f9f23
kellyn.li <kellynlee0011@outlook.com>
2022-05-11 23:01:34 +1000

    feature/backend server with part of vue pages

--- arduino_server/ml_model/ml_model.ino
+++ arduino_server/ml_model/ml_model.ino
@@ -8,19 +8,23 @@
 #include <Kellynnnnn-project-1_inferencing.h>
 #include <ArduinoJson.h>
 #include <WiFi.h>
-#include <WebServer.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
 #define FREQUENCY_HZ        60
 #define INTERVAL_MS         (1000 / (FREQUENCY_HZ + 1))
 
 // objeto da classe Adafruit_MPU6050
 Adafruit_MPU6050 mpu;
+int vib = 26;
 
 // Constants for WiFi
-const char *SSID = "your_wifi_ssid";
-const char *PWD = "your_wifi_pwd";
+const char *SSID = "Kellyn‘s Iphone";
+const char *PWD = "jellyapple";
 
-WebServer server(80);
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+StaticJsonDocument<100> doc; // Allocate a static JSON document
 
 float features[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE];
 size_t feature_ix = 0;
@@ -29,6 +33,10 @@ size_t feature_ix = 0;
 StaticJsonDocument<250> jsonDocument;
 char buffer[250];
 
+int endFlip = 0;
+int startFlip = 0;
+int decMode = 0;
+int mixMode = 0;
 
 static unsigned long last_interval_ms = 0;
 
@@ -51,49 +59,101 @@ void connectToWifi () {
   Serial.println(WiFi.RSSI());
 }
 
-void setupRouting() {     
-  Serial.print("Start listenting:");
-  server.on("/calibrate", onStartCalibrate);
-  server.on("/flip", onDetectFlip);
-  server.on("/vibrate", onStartVibrate);
-
-  // start server    
-  server.begin();    
+void notifyClients(String msg) {
+  ws.textAll(msg);
 }
 
 void onStartCalibrate () {
   Serial.println("start calibration");
-  createMsgJson("calibrate successful!");
-  server.send(200, "application/json", buffer);
+  // createMsgJson("calibrate successful!");
+  notifyClients("calibrating");
 }
 
 void onDetectFlip () {
   Serial.println("start detecting flip");
-  boolean result = checkMovement("Flip");
-  if (result) {
-    createMsgJson("detect fliping wrist!");
-    server.send(200, "application/json", buffer);
-  } else {
-    createMsgJson("nothing found! try again");
-    server.send(404, "application/json", buffer);
-  }
+  decMode = 1;
+  startFlip = 1;
+  
 }
 
 void onStartVibrate () {
+  digitalWrite(vib, HIGH);
+  delay(200);
+  digitalWrite(vib, LOW);
   Serial.println("start vibrating motor!");
-  createMsgJson("vibrate successful!");
-  server.send(200, "application/json", buffer);
+  notifyClients("vibrating");
 }
+
+void onStartMixing () {
+  Serial.println("start detecting mixing&circling");
+  mixMode = 1;
+}
+
+void onEndMixing() {
+  Serial.println("end mixing, stop detecting");
+  mixMode = 0;
+}
+
+//disable for now, maybe future needed
+// void vibrate(int x){
+//   for (int i=0; i<x; i++){
+//     digitalWrite(vib, HIGH);
+//     delay(200);
+//     digitalWrite(vib, LOW);
+//     delay(300);
+//   }
+// }
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  // Serial.print((char*)data);
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    Serial.print("websocket on listenting:");
+    if (strcmp((char*)data, "calibrate") == 0) {
+       onStartCalibrate();
+    } else if (strcmp((char*)data, "flip") == 0) {
+       onDetectFlip();
+    } else if (strcmp((char*)data, "vibrate") == 0) {
+      onStartVibrate();
+    } else if (strcmp((char*)data, "mix") == 0) {
+      onStartMixing();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
 void createMsgJson(char *msg) {  
   jsonDocument.clear();  
   jsonDocument["message"] = msg;
   serializeJson(jsonDocument, buffer);
 }
 
-boolean checkMovement(char* type) {
+void checkMovement(char* type) {
   sensors_event_t a, g, temp;
-  for (int i = 0; i < 500; i++) {
-    if (millis() > last_interval_ms + INTERVAL_MS) {
+  if (millis() > last_interval_ms + INTERVAL_MS) {
     last_interval_ms = millis();
 
     mpu.getEvent(&a, &g, &temp);
@@ -108,37 +168,38 @@ boolean checkMovement(char* type) {
       ei_impulse_result_t result;
       int err = numpy::signal_from_buffer(features, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
       if (err != 0) {
-        return false;
+        ei_printf("Failed to create signal from buffer (%d)\n", err);
+        return;
       }
 
       EI_IMPULSE_ERROR res = run_classifier(&signal, &result, true);
 
-      if (res != 0) return false;
-
+      if (res != 0) return;
       for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
         if (result.classification[ix].value > 0.6) {
-          if (result.classification[ix].label == type)
-          {
-            Serial.println("circle");
-            return true;
-            
-          } else if (result.classification[ix].label == type)
-          {
-            Serial.println("flip");
-            return true;
-          }else
-          {
-            Serial.println("nothing");
-            return false;
+          if (type == "Flip") {
+            if (result.classification[ix].label == type) {
+              // Flip action's case, only capture flip
+              notifyClients("detect flip");
+              if (endFlip == 0) {
+                endFlip = 1;
+              } else {
+                endFlip = 0;
+                startFlip = 0;
+              }
+            }
+          } else {
+            // Mix action's case, need to capture both normal and abnormal actions
+            if (result.classification[ix].label == "Circle" || result.classification[ix].label == "Mix"){
+               notifyClients("good");
+            } else if (result.classification[ix].label == "Mix_abnormal" || result.classification[ix].label == "Circle_abnormal") {
+                notifyClients("bad");
+            }
           }
-        } else {
-          //nothing found
-          return false;
         }
       }
       feature_ix = 0;
-      }
-    } 
+    }
   }
 }
 
@@ -214,13 +275,26 @@ void setupMpu() {
 
 void setup() {
   Serial.begin(115200);
+  pinMode(vib, OUTPUT);
   setupMpu();
   connectToWifi();
-  setupRouting();
+  initWebSocket();
+  server.begin();
   delay(100);
 
 }
 
 void loop() {
-  server.handleClient();
+  ws.cleanupClients();
+  if (decMode) {
+    if (startFlip == 1 && endFlip == 0) {
+    checkMovement("Flip");
+  }
+  if (startFlip == 1 && endFlip == 1) {
+    checkMovement("Flip");
+    } 
+  }
+  if (mixMode) {
+    checkMovement("MixHandling");
+  }
 }
---
+++ front_end_vue/src/page/HomePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="mb-5">
+      <img
+        src="https://i.ibb.co/6sGFkTz/Cakegif.gif"
+        class="img-fluid"
+        height=""
+        alt="Cupcake Animation Icon"
+      />
+    </div>
+
+    <div>
+      <h1 class="display-4 fw-bold">INFO90003</h1>
+    </div>
+
+    <div>
+      <div class="col-3 mx-auto py-5">
+        <div class="d-grid gap-4" id="home-nav">
+          <a
+            href="./page/calibration"
+            class="btn btn-primary btn-lg py-3 shadow-sm rounded-pill fs-3"
+            type="button"
+            >Piping</a
+          >
+          <!-- <a href="Calibration.html" class="btn btn-primary btn-lg py-3 shadow-sm rounded-pill fs-3" type="button">Mixing</a> -->
+          <!-- <a href="Calibration.html" class="btn btn-primary btn-lg py-3 shadow-sm rounded-pill fs-3" type="button">Pouring</a> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
